5a0029c0d9d32b7357d5e92d5019ec86452f0ac4
Lafie-rage <corentin.destrez@ig2i.centralelille.fr>
2021-12-02 22:17:29 +0100

    Mise à jour du script de génération de la base SEL_LA2_MONNAIE

--- SEL_LA2_MONNAIE_model.sql
+++ SEL_LA2_MONNAIE_model.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 25 nov. 2021 à 17:26
--- Version du serveur : 8.0.27
--- Version de PHP : 7.4.26
+-- Host: localhost:3306
+-- Generation Time: Dec 02, 2021 at 10:15 PM
+-- Server version: 8.0.27-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,92 +19,82 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `sel_la2_monnaie`
+-- Database: `SEL_LA2_Monnaie`
 --
-CREATE DATABASE IF NOT EXISTS `sel_la2_monnaie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `sel_la2_monnaie`;
+CREATE DATABASE IF NOT EXISTS `SEL_LA2_Monnaie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `SEL_LA2_Monnaie`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
+-- Table structure for table `categorie`
 --
 
 DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `idCategorie` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idCategorie`)
+CREATE TABLE `categorie` (
+  `idCategorie` int NOT NULL,
+  `description` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `competence`
+-- Table structure for table `competence`
 --
 
 DROP TABLE IF EXISTS `competence`;
-CREATE TABLE IF NOT EXISTS `competence` (
-  `idCompetence` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `competence` (
+  `idCompetence` int NOT NULL,
   `Description` varchar(100) DEFAULT NULL,
-  `Categorie_idCategorie` int NOT NULL,
-  PRIMARY KEY (`idCompetence`),
-  KEY `fk_Competence_Categorie1_idx` (`Categorie_idCategorie`)
+  `Categorie_idCategorie` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `competencemembre`
+-- Table structure for table `competencemembre`
 --
 
 DROP TABLE IF EXISTS `competencemembre`;
-CREATE TABLE IF NOT EXISTS `competencemembre` (
-  `Competence_idCompetence` int NOT NULL AUTO_INCREMENT,
-  `Membre_CodeMembre` int NOT NULL,
-  PRIMARY KEY (`Competence_idCompetence`,`Membre_CodeMembre`),
-  KEY `fk_CompetenceMembre_Competence1_idx` (`Competence_idCompetence`),
-  KEY `fk_CompetenceMembre_Membre1_idx` (`Membre_CodeMembre`)
+CREATE TABLE `competencemembre` (
+  `Competence_idCompetence` int NOT NULL,
+  `Membre_CodeMembre` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cotisation`
+-- Table structure for table `cotisation`
 --
 
 DROP TABLE IF EXISTS `cotisation`;
-CREATE TABLE IF NOT EXISTS `cotisation` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cotisation` (
   `DateDebut` datetime NOT NULL,
   `Prix` int NOT NULL,
-  `DateFin` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `DateFin` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `espacedonnees`
+-- Table structure for table `espacedonnees`
 --
 
 DROP TABLE IF EXISTS `espacedonnees`;
-CREATE TABLE IF NOT EXISTS `espacedonnees` (
+CREATE TABLE `espacedonnees` (
   `Contenu` varchar(255) DEFAULT NULL,
-  `Membre_CodeMembre` int NOT NULL,
-  PRIMARY KEY (`Membre_CodeMembre`),
-  KEY `fk_EspaceDonnees_Membre1_idx` (`Membre_CodeMembre`)
+  `Membre_CodeMembre` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `membre`
+-- Table structure for table `membre`
 --
 
 DROP TABLE IF EXISTS `membre`;
-CREATE TABLE IF NOT EXISTS `membre` (
-  `CodeMembre` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `membre` (
+  `CodeMembre` int NOT NULL,
   `Nom` varchar(100) DEFAULT NULL,
   `Prenom` varchar(100) DEFAULT NULL,
   `Rue` varchar(100) DEFAULT NULL,
@@ -115,152 +106,266 @@ CREATE TABLE IF NOT EXISTS `membre` (
   `CompteTemps` int DEFAULT NULL,
   `habitantParc` tinyint(1) NOT NULL DEFAULT '0',
   `nom_de_commerce` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `solde_ecu` float NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CodeMembre`)
+  `solde_ecu` float NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `membre_has_cotisation`
+-- Table structure for table `membre_has_cotisation`
 --
 
 DROP TABLE IF EXISTS `membre_has_cotisation`;
-CREATE TABLE IF NOT EXISTS `membre_has_cotisation` (
+CREATE TABLE `membre_has_cotisation` (
   `Membre_CodeMembre` int NOT NULL,
   `DatePaiement` datetime DEFAULT NULL,
-  `Id_Membre_has_Cotisationcol` int NOT NULL,
-  PRIMARY KEY (`Id_Membre_has_Cotisationcol`),
-  KEY `FK_Membre_has_Cotisation_Membre_id` (`Membre_CodeMembre`) USING BTREE
+  `Id_Membre_has_Cotisationcol` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `operation`
+-- Table structure for table `operation`
 --
 
 DROP TABLE IF EXISTS `operation`;
-CREATE TABLE IF NOT EXISTS `operation` (
-  `idOperation` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `operation` (
+  `idOperation` int NOT NULL,
   `codeDebiteur` int NOT NULL,
   `codeCrediteur` int NOT NULL,
   `dateHeureOperation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `montantEcu` float NOT NULL,
-  PRIMARY KEY (`idOperation`),
-  KEY `FK_Operation_debiteur_code` (`codeDebiteur`),
-  KEY `FK_Operation_crediteur_code` (`codeCrediteur`)
+  `montantEcu` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `operationchange`
+-- Table structure for table `operationchange`
 --
 
 DROP TABLE IF EXISTS `operationchange`;
-CREATE TABLE IF NOT EXISTS `operationchange` (
-  `idChange` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `operationchange` (
+  `idChange` int NOT NULL,
   `montant` float NOT NULL,
   `type` varchar(10) NOT NULL,
   `Membre_CodeMembre` int NOT NULL,
   `dateHeureChange` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `soldeCompte` float NOT NULL DEFAULT '0',
-  PRIMARY KEY (`idChange`),
-  KEY `fk_Operation_Change_Membre` (`Membre_CodeMembre`)
+  `soldeCompte` float NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `proposition`
+-- Table structure for table `proposition`
 --
 
 DROP TABLE IF EXISTS `proposition`;
-CREATE TABLE IF NOT EXISTS `proposition` (
+CREATE TABLE `proposition` (
   `idProposition` int NOT NULL,
   `Competence_idCompetence` int NOT NULL,
   `Description` varchar(100) DEFAULT NULL,
   `DateDebut` datetime DEFAULT NULL,
   `DateFin` datetime DEFAULT NULL,
-  `Membre_CodeMembre` int NOT NULL,
-  PRIMARY KEY (`idProposition`,`Competence_idCompetence`),
-  KEY `fk_Proposition_Competence1_idx` (`Competence_idCompetence`),
-  KEY `fk_Proposition_Membre1_idx` (`Membre_CodeMembre`)
+  `Membre_CodeMembre` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `transaction`
+-- Table structure for table `transaction`
 --
 
 DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE IF NOT EXISTS `transaction` (
-  `idTransaction` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `transaction` (
+  `idTransaction` int NOT NULL,
   `Membre_CodeMembre` int NOT NULL,
   `Etat` varchar(45) DEFAULT NULL,
   `Proposition_idProposition` int NOT NULL,
   `DureeInitiale` int DEFAULT NULL,
   `DureeEffective` int DEFAULT NULL,
   `DatePrevisionnelle` datetime DEFAULT NULL,
-  `DateRealisation` datetime DEFAULT NULL,
-  PRIMARY KEY (`idTransaction`),
-  KEY `fk_Transaction_Membre1_idx` (`Membre_CodeMembre`),
-  KEY `fk_Transaction_Proposition1_idx` (`Proposition_idProposition`)
+  `DateRealisation` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `competence`
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`idCategorie`);
+
+--
+-- Indexes for table `competence`
+--
+ALTER TABLE `competence`
+  ADD PRIMARY KEY (`idCompetence`),
+  ADD KEY `fk_Competence_Categorie1_idx` (`Categorie_idCategorie`);
+
+--
+-- Indexes for table `competencemembre`
+--
+ALTER TABLE `competencemembre`
+  ADD PRIMARY KEY (`Competence_idCompetence`,`Membre_CodeMembre`),
+  ADD KEY `fk_CompetenceMembre_Competence1_idx` (`Competence_idCompetence`),
+  ADD KEY `fk_CompetenceMembre_Membre1_idx` (`Membre_CodeMembre`);
+
+--
+-- Indexes for table `cotisation`
+--
+ALTER TABLE `cotisation`
+  ADD PRIMARY KEY (`DateDebut`);
+
+--
+-- Indexes for table `espacedonnees`
+--
+ALTER TABLE `espacedonnees`
+  ADD PRIMARY KEY (`Membre_CodeMembre`),
+  ADD KEY `fk_EspaceDonnees_Membre1_idx` (`Membre_CodeMembre`);
+
+--
+-- Indexes for table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`CodeMembre`);
+
+--
+-- Indexes for table `membre_has_cotisation`
+--
+ALTER TABLE `membre_has_cotisation`
+  ADD PRIMARY KEY (`Id_Membre_has_Cotisationcol`),
+  ADD KEY `FK_Membre_has_Cotisation_Membre_id` (`Membre_CodeMembre`) USING BTREE;
+
+--
+-- Indexes for table `operation`
+--
+ALTER TABLE `operation`
+  ADD PRIMARY KEY (`idOperation`),
+  ADD KEY `FK_Operation_debiteur_code` (`codeDebiteur`),
+  ADD KEY `FK_Operation_crediteur_code` (`codeCrediteur`);
+
+--
+-- Indexes for table `operationchange`
+--
+ALTER TABLE `operationchange`
+  ADD PRIMARY KEY (`idChange`),
+  ADD KEY `fk_Operation_Change_Membre` (`Membre_CodeMembre`);
+
+--
+-- Indexes for table `proposition`
+--
+ALTER TABLE `proposition`
+  ADD PRIMARY KEY (`idProposition`,`Competence_idCompetence`),
+  ADD KEY `fk_Proposition_Competence1_idx` (`Competence_idCompetence`),
+  ADD KEY `fk_Proposition_Membre1_idx` (`Membre_CodeMembre`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`idTransaction`),
+  ADD KEY `fk_Transaction_Membre1_idx` (`Membre_CodeMembre`),
+  ADD KEY `fk_Transaction_Proposition1_idx` (`Proposition_idProposition`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `idCategorie` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `competence`
+--
+ALTER TABLE `competence`
+  MODIFY `idCompetence` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `competencemembre`
+--
+ALTER TABLE `competencemembre`
+  MODIFY `Competence_idCompetence` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `membre`
+--
+ALTER TABLE `membre`
+  MODIFY `CodeMembre` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `operation`
+--
+ALTER TABLE `operation`
+  MODIFY `idOperation` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `operationchange`
+--
+ALTER TABLE `operationchange`
+  MODIFY `idChange` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `idTransaction` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `competence`
 --
 ALTER TABLE `competence`
   ADD CONSTRAINT `fk_Competence_Categorie1` FOREIGN KEY (`Categorie_idCategorie`) REFERENCES `categorie` (`idCategorie`);
 
 --
--- Contraintes pour la table `competencemembre`
+-- Constraints for table `competencemembre`
 --
 ALTER TABLE `competencemembre`
   ADD CONSTRAINT `fk_CompetenceMembre_Competence1` FOREIGN KEY (`Competence_idCompetence`) REFERENCES `competence` (`idCompetence`),
   ADD CONSTRAINT `fk_CompetenceMembre_Membre1` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`);
 
 --
--- Contraintes pour la table `espacedonnees`
+-- Constraints for table `espacedonnees`
 --
 ALTER TABLE `espacedonnees`
   ADD CONSTRAINT `fk_EspaceDonnees_Membre1` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`);
 
 --
--- Contraintes pour la table `membre_has_cotisation`
+-- Constraints for table `membre_has_cotisation`
 --
 ALTER TABLE `membre_has_cotisation`
   ADD CONSTRAINT `FK_Membre_has_Cotisation_Membre` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Contraintes pour la table `operation`
+-- Constraints for table `operation`
 --
 ALTER TABLE `operation`
   ADD CONSTRAINT `FK_Operation_crediteur_code` FOREIGN KEY (`codeCrediteur`) REFERENCES `membre` (`CodeMembre`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_Operation_debiteur_code` FOREIGN KEY (`codeDebiteur`) REFERENCES `membre` (`CodeMembre`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `operationchange`
+-- Constraints for table `operationchange`
 --
 ALTER TABLE `operationchange`
   ADD CONSTRAINT `fk_Operation_Change_Membre` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `proposition`
+-- Constraints for table `proposition`
 --
 ALTER TABLE `proposition`
   ADD CONSTRAINT `fk_Proposition_Competence1` FOREIGN KEY (`Competence_idCompetence`) REFERENCES `competence` (`idCompetence`),
   ADD CONSTRAINT `fk_Proposition_Membre1` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`);
 
 --
--- Contraintes pour la table `transaction`
+-- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
   ADD CONSTRAINT `fk_Transaction_Membre1` FOREIGN KEY (`Membre_CodeMembre`) REFERENCES `membre` (`CodeMembre`),
